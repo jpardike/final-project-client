ed6d7d84da07888528b7bc6a9287af6c8217b514
jpardike <jpardike@gmail.com>
2020-12-02 01:10:49 -0800

    Adds blank landing page and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+import Navbar from './components/navbar/Navbar';
+import Landing from './pages/landing/Landing';
+
 function App() {
   return (
-    <div className="container-fluid">
-      
+    <div className="container-fluid p-0">
+      <Navbar />
+      <Landing />
     </div>
   );
 }
