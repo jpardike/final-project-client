ad80055f79ce3eb7392ef22f33355f49a34d27f4
jpardike <jpardike@gmail.com>
2020-12-03 00:09:37 -0800

    Changes new user route and adds user profile page

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,16 +2,15 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Landing from "../pages/landing/Landing";
-import About from "../pages/about/About";
-import NewUser from "../pages/newUser/NewUser";
 import Feed from "../pages/feed/Feed";
+import UserProfile from "../pages/userProfile/UserProfile";
+import About from "../pages/about/About";
 
 export default (
   <Switch>
     <Route exact path="/" component={Landing} />
     <Route exact path="/about" component={About} />
     <Route exact path="/feed" component={Feed} />
-    <Route exact pathe="/user/new" component={NewUser} />
-
+    <Route path="/user/:id" component={UserProfile} />
   </Switch>
 );
---
+++ src/pages/userProfile/UserProfile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './userProfile.css';
+
+class UserProfile extends React.Component {
+
+  render() {
+    return (
+      <div>
+        User Profile
+      </div>
+    )
+  }
+}
+
+export default UserProfile;
