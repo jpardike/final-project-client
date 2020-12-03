a47f225cac459bac801f64d9158d6d1760fdd602
jpardike <jpardike@gmail.com>
2020-12-02 16:30:13 -0800

    Adds feed page that renders postsCards components

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,10 +3,13 @@ import { Switch, Route } from "react-router-dom";
 
 import Landing from "../pages/landing/Landing";
 import NewUser from "../pages/newUser/NewUser";
+import Feed from "../pages/feed/Feed";
 
 export default (
   <Switch>
     <Route exact path="/" component={Landing} />
+    <Route exact path="/feed" component={Feed} />
     <Route exact pathe="/user/new" component={NewUser} />
+
   </Switch>
 );
---
+++ src/pages/feed/Feed.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import PostsCards from '../../components/postsCards/PostsCards';
+
+import './feed.css';
+
+class Feed extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <PostsCards />
+      </div>
+    )
+  }
+}
+
+export default Feed;
