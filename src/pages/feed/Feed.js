b9e3b839bddec6585c6b32d060040863e58b3e4e
jpardike <jpardike@gmail.com>
2020-12-06 04:00:30 -0800

    Shows all posts on users feed page

--- src/pages/feed/Feed.js
+++ src/pages/feed/Feed.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React from "react";
+import PostModel from "../../models/post";
 
-// import PostsCards from '../../components/postsCards/PostsCards';
+import PostsCards from "../../components/postsCards/PostsCards";
 
-import './feed.css';
+import "./feed.css";
 
 class Feed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    PostModel.all().then((data) => {
+      this.setState({ posts: data.posts, loading: false });
+    });
+  }
+
+  renderPostsCards() {
+    return this.state.posts.map((post) => {
+      return <PostsCards key={post._id} post={post} />;
+    });
+  }
 
   render() {
-    return (
-      <div>
-        {/* <PostsCards /> */}
-      </div>
-    )
+    if (this.state.loading) {
+      return (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      );
+    } else {
+      return <div>{this.renderPostsCards()}</div>;
+    }
   }
 }
 
