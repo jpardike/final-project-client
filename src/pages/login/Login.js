8afbaa6713a049f0a7c2a2b0e37a2a106e30a469
jpardike <jpardike@gmail.com>
2020-12-06 17:34:37 -0800

    Adds functionality to user login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,15 +1,58 @@
-import React from 'react';
+import React from "react";
+import UserModel from "../../models/user";
 
-import './login.css';
+
+import "./login.css";
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleLoginInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleLoginFormSubmit = (event) => {
+    event.preventDefault();
+    UserModel.login({email: this.state.email}).then((data) => {
+      this.props.changeUser(data.user._id);
+      this.props.history.push(`/user/feed/${data.user._id}`);
+    });
+  }
 
   render() {
     return (
       <div>
-
+        <form onSubmit={this.handleLoginFormSubmit}>
+          <div className="row justify-content-center">
+            <div className="col text-center">
+              <label htmlFor="email">Email</label>
+              <input
+                onChange={this.handleLoginInputChange}
+                value={this.state.email}
+                type="email"
+                name="email"
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                onChange={this.handleLoginInputChange}
+                value={this.state.password}
+                type="password"
+                name="password"
+              />
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <input value="Login" type="submit" className="btn btn-primary" />
+          </div>
+        </form>
       </div>
-    )
+    );
   }
 }
 
