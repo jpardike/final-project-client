82626ba24995d34fcdf4b7ef4c97de83bc11804e
jpardike <jpardike@gmail.com>
2020-12-07 05:44:25 -0800

    Basic styling done to landing page

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -5,22 +5,20 @@ import { Link } from "react-router-dom";
 import "./landing.css";
 
 function Landing(props) {
-    return (
-      <div className="row p-0 m-0">
-        <div className="col-md text-center">COMPANY LOGO</div>
-        <div className="col-md text-center">
-          <div>Brief description of site</div>
-          <div>
-            <Link to="/user/new" className="btn">
-              SIGNUP
-            </Link>
-            <Link to="/user/login" className="btn">
-              LOGIN
-            </Link>
-          </div>
-        </div>
+  return (
+    <div className="row p-0 m-0 align-items-center landing-body">
+      <div className="col-md text-center landing-col">COMPANY LOGO</div>
+      <div className="col-md text-center landing-col">
+        <p>Brief description of site</p>
+        <Link to="/user/new" className="btn mr-4">
+          SIGNUP
+        </Link>
+        <Link to="/user/login" className="btn ml-4">
+          LOGIN
+        </Link>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Landing;
