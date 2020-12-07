c77f755666184f0ce986f8d056be3fc478faa07f
jpardike <jpardike@gmail.com>
2020-12-07 10:38:53 -0800

    Minor adjustments

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -46,7 +46,7 @@ class Navbar extends React.Component {
     return (
       <nav className="navbar navbar-expand-md">
         <Link to="/" className="navbar-brand" id="nav-text">
-          ICON
+          BANDMATES
         </Link>
         <button
           className="navbar-toggler"
--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -6,10 +6,10 @@ import "./landing.css";
 
 function Landing(props) {
   return (
-    <div className="row p-0 m-0 align-items-center landing-body">
-      <div className="col-md text-center landing-col">COMPANY LOGO</div>
-      <div className="col-md text-center landing-col">
-        <p>Brief description of site</p>
+    <div className="row p-0 m-0 align-items-center landing-body justify-content-around">
+      <div className="col-md-5 text-center landing-col landing-logo">COMPANY LOGO</div>
+      <div className="col-md-5 text-center landing-col">
+        <p className="py-3">BandMates is a social media app for musicians and music lovers who are looking for upcoming events and new music from other musicians.</p>
         <Link to="/user/new" className="btn mr-4">
           SIGNUP
         </Link>
