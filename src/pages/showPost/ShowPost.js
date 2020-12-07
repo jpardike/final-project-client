b2b0fcea4ee97e248a00d0f2fd7389694d563c16
jpardike <jpardike@gmail.com>
2020-12-07 03:16:51 -0800

    Show post page now shows the post

--- src/pages/showPost/ShowPost.js
+++ src/pages/showPost/ShowPost.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import './showPost.css';
+import PostModel from "../../models/post";
+
+import "./showPost.css";
 
 class ShowPost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: {},
+    };
+  }
+
+  componentDidMount() {
+    const postId = this.props.match.params.id;
+    PostModel.getOne(postId).then((data) => {
+      this.setState({ post: data.post });
+    });
+  }
 
   render() {
     return (
-      <div>
-        <p>Show Post Page</p>
+      <div className="card">
+        <div className="card-body">
+          <p>{this.state.post.body}</p>
+        </div>
       </div>
-    )
+    );
   }
 }
 
