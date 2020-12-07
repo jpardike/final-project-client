4df9a741e9617a18d9c41ade1d1c8e8fe6c93581
jpardike <jpardike@gmail.com>
2020-12-07 08:00:59 -0800

    Basic styling for show post page

--- src/pages/showPost/ShowPost.js
+++ src/pages/showPost/ShowPost.js
@@ -25,29 +25,44 @@ class ShowPost extends React.Component {
     const postId = this.props.match.params.id;
     PostModel.destroy(postId).then((data) => {
       this.props.history.goBack();
-    })
-  }
+    });
+  };
 
   render() {
     if (this.props.currentUser === this.state.postUserId) {
       return (
-        <div className="card">
-          <div className="card-body">
-            <p>{this.state.post.body}</p>
-            <div className="row p-0 m-0">
-              <Link className="btn" to={`/post/update/${this.state.post._id}`}>
-              Update
-              </Link>
-              <button className="btn" onClick={this.deletePostClick}>Delete</button>
+        <div className="row p-0 m-0 justify-content-center">
+          <div className="col-6 mt-5 text-center">
+            <h2 className="mb-5">View Post</h2>
+            <div className="card">
+              <div className="card-body">
+                <p className="m-4">{this.state.post.body}</p>
+                <div className="row p-0 m-0 justify-content-around">
+                  <Link
+                    className="btn"
+                    to={`/post/update/${this.state.post._id}`}
+                  >
+                    Update
+                  </Link>
+                  <button className="btn" onClick={this.deletePostClick}>
+                    Delete
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       );
     } else {
       return (
-        <div className="card">
-          <div className="card-body">
-            <p>{this.state.post.body}</p>
+        <div className="row p-0 m-0 justify-content-center">
+          <div className="col-6 text-center">
+            <h2 className="mb-5">View Post</h2>
+            <div className="card">
+              <div className="card-body">
+                <p className="m-4">{this.state.post.body}</p>
+              </div>
+            </div>
           </div>
         </div>
       );
